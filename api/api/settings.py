910fdb7fff6e8cd5dcb4eee31969d06fd326ab93
Jay Khatri <khatri.florida@gmail.com>
2019-07-19 21:13:30 +0100

    api endpoint works

--- api/api/settings.py
+++ api/api/settings.py
@@ -27,10 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Rest framework configuration.
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
